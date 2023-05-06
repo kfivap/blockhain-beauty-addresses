a687662fdd3745354f9675a467c662ab9c0c6631
kfivap <kfivap@gmail.com>
2023-05-06 03:47:10 +0300

    go

---
+++ README.md
@@ -0,0 +1,13 @@
+# GO
+```bash
+cd go
+go build find_addresses.go
+./find_addresses.exe
+```
+
+
+# NODE
+```bash
+cd js
+node src/index.js
+```
---
+++ go/find_addresses.go
@@ -0,0 +1,144 @@
+package main
+
+import (
+	"crypto/rand"
+	"crypto/sha256"
+	"encoding/hex"
+	"encoding/json"
+	"fmt"
+	"os"
+	"strings"
+)
+
+type Address struct {
+	Score      int
+	Address    string
+	PrivateKey string
+}
+
+func areLastNCharsSame(str string, n int) bool {
+	lastNChars := str[len(str)-n:]
+	lastChar := strings.ToLower(string(lastNChars[0]))
+	return strings.Count(lastNChars, lastChar) == n
+}
+
+func areFirstNCharsSame(str string, n int) bool {
+	firstNChars := str[2 : n+2]
+	firstChar := strings.ToLower(string(firstNChars[0]))
+	return strings.Count(firstNChars, firstChar) == n
+}
+
+func hasMoreThanNRepeatedCharsInARow(str string, N int) bool {
+	repeated := string(str[0])
+	for i := 1; i < len(str); i++ {
+		if str[i] == repeated[len(repeated)-1] {
+			repeated += string(str[i])
+			if len(repeated) == N {
+				return true
+			}
+		} else {
+			repeated = string(str[i])
+		}
+	}
+	return false
+}
+
+func scoreAddress(address string, numberChars int) int {
+	score := 0
+	hasRepeatedChars := hasMoreThanNRepeatedCharsInARow(address, numberChars)
+	if hasRepeatedChars {
+		firstCharsSame := areFirstNCharsSame(address, numberChars)
+		lastCharsSame := areLastNCharsSame(address, numberChars)
+		if firstCharsSame && lastCharsSame {
+			score += 100
+		} else if firstCharsSame {
+			score += 5
+		} else if lastCharsSame {
+			score += 10
+		} else {
+			// score += 1 // uncomment if need chars in the middle
+		}
+	}
+	return score
+}
+
+func computeAddress(privateKey []byte) string {
+	hasher := sha256.New()
+	hasher.Write(privateKey)
+	hash := hasher.Sum(nil)
+
+	// Compute the address from the public key by taking the last 20 bytes
+	// of the keccak256 hash of the public key.
+	hasher = sha256.New()
+	hasher.Write(hash)
+	publicKeyHash := hasher.Sum(nil)
+
+	hasher = sha256.New()
+	hasher.Write(publicKeyHash)
+	publicKeyHash = hasher.Sum(nil)
+
+	keccakHasher := sha256.New()
+	keccakHasher.Write(publicKeyHash)
+	hash = keccakHasher.Sum(nil)
+
+	address := hex.EncodeToString(hash[len(hash)-20:])
+
+	return address
+}
+
+func generateAddresses(numberChars int, out chan<- Address) {
+	counter := 0
+	for {
+		privateKey := make([]byte, 32)
+		_, err := rand.Read(privateKey)
+		if err != nil {
+			panic(err)
+		}
+		address := computeAddress(privateKey)
+		address = strings.ToLower(address)
+		score := scoreAddress(address, numberChars)
+		if score != 0 {
+			out <- Address{
+				Score:      score,
+				Address:    address,
+				PrivateKey: hex.EncodeToString(privateKey),
+			}
+		}
+
+		counter++
+		if counter%1000000 == 0 {
+			fmt.Printf("Processed %d keys\n", counter)
+		}
+	}
+}
+
+func main() {
+	numberChars := 6
+	numWorkers := 4
+	out := make(chan Address, 1000)
+	for i := 0; i < numWorkers; i++ {
+		go generateAddresses(numberChars, out)
+	}
+
+	outputFile, err := os.OpenFile("beauty_addresses.json", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		panic(err)
+	}
+	defer outputFile.Close()
+
+	counter := 0
+	for {
+		select {
+		case addr := <-out:
+			counter++
+			fmt.Printf("%+v\n", addr)
+			jsonStr, err := json.Marshal(addr)
+			if err != nil {
+				fmt.Printf("Error marshalling address %+v: %v\n", addr, err)
+				continue
+			}
+			outputFile.Write(jsonStr)
+			outputFile.Write([]byte("\n"))
+		}
+	}
+}
