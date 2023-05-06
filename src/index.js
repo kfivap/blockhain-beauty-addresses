5929ae5f412fa1dc3ae4a88bfb7dbb79b20defd8
kfivap <kfivap@gmail.com>
2023-05-06 03:00:24 +0300

    init

---
+++ src/index.js
@@ -0,0 +1,41 @@
+const { Worker } = require("worker_threads");
+const fs = require('fs')
+
+function main() {
+  const numThreads = 4;
+  const threads = [];
+
+  for (let i = 0; i < numThreads; i++) {
+
+    const worker = new Worker(__dirname + "/worker.js", {
+      workerData: {
+        numberChars: 3,
+        limit: 10000
+      },
+    });
+
+    worker.on('error', err => {
+      console.error(err);
+    });
+
+    worker.on("message", (msg) => {
+      const { type, message } = msg
+      if (type === 'log') {
+        console.log(message)
+      } else if (type === 'data') {
+        fs.appendFileSync(__dirname + '/beauty_addresses.json', JSON.stringify(message) + '\n')
+      } else {
+        console.error('upsupported msg', msg)
+      }
+    });
+
+    threads.push(worker);
+  }
+
+  threads.forEach(worker => {
+    worker.on('exit', () => {
+      console.log('Worker has exited.');
+    });
+  });
+}
+main()
---
+++ src/worker.js
@@ -0,0 +1,65 @@
+const { parentPort, workerData, threadId } = require("worker_threads");
+const ethers = require('ethers')
+
+function areLastNCharsSame(str, n) {
+    const lastNChars = str.slice(-n);
+    const lastChar = lastNChars.charAt(0).toLowerCase();
+    return lastNChars.split('').every(char => char === lastChar);
+}
+
+function areFirstNCharsSame(str, n) {
+    const slicePrefix = 2
+    const firstNChars = str.slice(slicePrefix, n + slicePrefix);
+    const firstChar = firstNChars.charAt(0).toLowerCase();
+    return firstNChars.split('').every(char => char === firstChar);
+}
+
+function hasMoreThanNRepeatedCharsInARow(str, N) {
+    const regex = new RegExp(`(.)\\1{${N - 1},}`, 'g');
+    return regex.test(str);
+}
+
+function scoreAddress(address, numberChars) {
+    let score = 0
+    const hasRepeatedChars = hasMoreThanNRepeatedCharsInARow(address, numberChars)
+    if (hasRepeatedChars) {
+        const firstCharsSame = areFirstNCharsSame(address, numberChars)
+        const lastCharsSame = areLastNCharsSame(address, numberChars)
+        if (firstCharsSame && lastCharsSame) {
+            score += 100
+        } else if (firstCharsSame) {
+            score += 5
+        } else if (lastCharsSame) {
+            score += 10
+        } else {
+            // score += 1 // uncomment if need chars in the middle
+        }
+
+    }
+    return score
+
+}
+
+function findAddresses(numberChars, limit) {
+    const startAt = Date.now()
+    const logEvery = 1000
+    let foundAddresses = 0
+    for (let i = 0; i < limit; i++) {
+        if (i % logEvery === 0) {
+            const msRun = Date.now() - startAt
+            parentPort.postMessage({type: 'log', message: `=== threadId: ${threadId}, running: ${((msRun) / 1000).toFixed()}s. processed ${i} keys, found ${foundAddresses} total addresses ===` })
+
+        }
+        const privateKey = ethers.hexlify(ethers.randomBytes(32));
+        const address = ethers.computeAddress(privateKey).toLowerCase();
+        const score = scoreAddress(address, numberChars)
+        if (score) {
+            foundAddresses++
+            const data = { score, address, privateKey }
+            parentPort.postMessage({type: 'data', message: data})
+        }
+
+    }
+}
+
+findAddresses(workerData.numberChars, workerData.limit)
